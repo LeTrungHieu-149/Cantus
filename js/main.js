c68aea01b0aa45b3979297fab6df6cccefc73e33
LeTrungHieu-149 <61939555+LeTrungHieu-149@users.noreply.github.com>
2020-04-03 17:05:31 +0700

    responsive

--- js/main.js
+++ js/main.js
@@ -1,7 +1,22 @@
+var x=0;
+var y=0;
+
 function openNav() {
     document.getElementById("menu").style.width="100%";
 }
 
 function closeNav() {
     document.getElementById("menu").style.width="0%";
+}
+
+function toggleSocial() {
+    x++;
+    if(x%2!=0) document.getElementById("social-mobile").style.height="300%";
+    else document.getElementById("social-mobile").style.height="0%";
+}
+
+function toggleStat() {
+    y++;
+    if(y%2!=0) document.getElementById("music-player__stat").style.height="100%";
+    else document.getElementById("music-player__stat").style.height="0%";
 }
