fc14d75a54b083b274a5966787b270bc3ab6c48e
LeTrungHieu-149 <61939555+LeTrungHieu-149@users.noreply.github.com>
2020-04-28 20:10:45 +0700

    reponsive problem fixed

--- js/main.js
+++ js/main.js
@@ -1,18 +1,15 @@
-var x=0;
 var y=0;
 
 function openNav() {
-    document.getElementById("menu").style.width="100%";
+    document.getElementById("menu").classList.add("menu--open");
 }
 
 function closeNav() {
-    document.getElementById("menu").style.width="0%";
+    document.getElementById("menu").classList.remove("menu--open");
 }
 
-function toggleSocial() {
-    x++;
-    if(x%2!=0) document.getElementById("social-mobile").style.height="300%";
-    else document.getElementById("social-mobile").style.height="0%";
+function toggleNav() {
+    document.querySelector(".nav__social-share").classList.toggle("social-share-show");
 }
 
 function toggleStat() {
