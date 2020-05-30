a368305a0abb0a84acbea8b5073d04130b6ca807
LeTrungHieu-149 <61939555+LeTrungHieu-149@users.noreply.github.com>
2020-05-30 19:50:16 +0700

    awdwa

--- js/main.js
+++ js/main.js
@@ -1,11 +1,28 @@
+function fadeAndHide(callback){
+    fade();
+    setTimeout(callback,2000);
+};
+    
+function hideLoader() {
+    document.querySelector(".loader").style.display="none";
+};
+    
+function fade() {
+    document.querySelector(".loader").style.opacity="0";
+}
+  
+window.onload=fadeAndHide(hideLoader);
+
 var y=0;
 
 function openNav() {
     document.getElementById("menu").classList.add("menu--open");
+    document.getElementsByTagName("body")[0].classList.add("no-scroll");
 }
 
 function closeNav() {
     document.getElementById("menu").classList.remove("menu--open");
+    document.getElementsByTagName("body")[0].classList.remove("no-scroll");
 }
 
 function toggleNav() {
